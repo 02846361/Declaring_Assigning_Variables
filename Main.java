845a9d0a3bb4cf1414a6ebed7e74f1837a12d291
02846361 <70173098+02846361@users.noreply.github.com>
2020-08-31 16:20:15 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int score = 50;
+	float price = 24.99f;
+	String user_name = "Bobby Johnson";
+
+	System.out.println("Test Score initialized is: " + score);
+	System.out.println("Price tag: " + price);
+	System.out.println("Username: " + user_name);
+    }
+}
